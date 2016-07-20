7e1d5fbc557b751a0ccacb88c2d8f777207dbab3
Rebecca Jahnke <rsjahnke@bu.edu>
2016-07-19 23:27:36 -0400

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Rebecca's Online Store, Inc.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Rebecca's Online Store</title>
+    <link rel="stylesheet" type="text/css"
+          href="/CS602/CS602_FinalProject_Jahnke/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1><a href="/CS602/CS602_FinalProject_Jahnke">Rebecca's Online Store<a/></h1></header>
+<!--<?php session_start(); ?>-->
